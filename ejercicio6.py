0146298242d7c5aecdceb9d3b9d8788e20aba1cd
Bastian Alessandro Gallegos <bastian4le@gmail.com>
2024-10-18 13:12:02 -0600

    Ejercico 6

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,8 +1,5 @@
-N1 = input('Ingresa el primer número: ')
-N1 = int(N1)
-N2 = input('Ingresa el segundo número: ')
-N2 = int(N2)
-N3 = input('Ingresa el tercer número: ')
-N3 = int(N3)
+N1 = float(input('Ingresa el primer número: '))
+N2 = float(input('Ingresa el segundo número: '))
+N3 = float(input('Ingresa el tercer número: '))
 promedio = (N1+N2+N3)/3
 print("El promedio es de:",promedio)
