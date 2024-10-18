5a24bad06b8173dcad2c706dcfb69b0056e71724
Bastian Alessandro Gallegos <bastian4le@gmail.com>
2024-10-18 13:14:27 -0600

    Ejerciccio 3

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,7 +1,5 @@
 import math
-catetoA = input('Cuanto mide el primer cateto: ')
-catetoA =int(catetoA)
-catetoB = input('Cuanto mide el segundo cateto: ')
-catetoB =int(catetoB)
+catetoA = float(input('Cuanto mide el primer cateto: '))
+catetoB = float(input('Cuanto mide el segundo cateto: '))
 hipotenusa = math.sqrt(catetoA**2 + catetoB**2)
 print("La hipotenusa es:", hipotenusa)
